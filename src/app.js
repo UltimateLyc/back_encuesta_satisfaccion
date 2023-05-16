2ce92c103bb1f662fb99116a0df050dafc269698
Rafael Ramos <ingrnrg@gmail.com>
2023-05-16 00:33:34 -0600

    feat: added middleware for cath errors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import errorHandler from './middleware/errorMiddleware.js'
 
 const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json)
+
+app.use(errorHandler)
 
 export default app
