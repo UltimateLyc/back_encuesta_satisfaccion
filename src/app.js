1eaf27661be88fc1b60c86068f440ae52f10537b
Rafael Ramos <ingrnrg@gmail.com>
2023-05-18 11:29:28 -0600

    fix: try to fix a bug with cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { commentRoutes, reactionRoutes } from './config/routes.config.js'
 import errorHandler from './middleware/errorMiddleware.js'
 
@@ -6,7 +7,7 @@ const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
+app.use(cors())
 // Endpoints
 app.use(reactionRoutes)
 app.use(commentRoutes)
